45e5d24a72679e0b0bcdfb4a5c7254cb07ff9eb0
Vyshnav67 <vyshnavanchery2015@gmail.com>
2023-05-08 17:12:44 +0530

    FIrst Commit

---
+++ Script/script.js
@@ -0,0 +1,48 @@
+// Accordion IMG
+function img1(){
+  document.getElementById('changing').src="/public/images/Accordion/basic_hero_ui@2x.png";
+  document.getElementById('sideimg').style.backgroundColor="aqua";
+}
+function img2(){
+  document.getElementById('changing').src="/public/images/Accordion/plus_plank_02_ui@2x.png";
+  document.getElementById('sideimg').style.backgroundColor="#fa551e";
+}
+function img3(){
+  document.getElementById('changing').src="/public/images/Accordion/CollabBrowse_2@2x.webp";
+  document.getElementById('sideimg').style.backgroundColor="#fad24b";
+}
+function img4(){
+  document.getElementById('changing').src="/public/images/Accordion/collaboration-diagram.svg";
+  document.getElementById('changing').style.width="370px";
+  document.getElementById('sideimg').style.backgroundColor="#b4c8e1";
+  
+}
+function img5(){
+  document.getElementById('changing').src="/public/images/Accordion/JTBD_UI_7@2x.webp";
+  document.getElementById('sideimg').style.backgroundColor="#c8aff0";
+  document.getElementById('changing').style.width="370px";
+}
+function img6(){
+  document.getElementById('changing').src="/public/images/Accordion/pro_plank_01_ui@2x.png";
+  document.getElementById('sideimg').style.backgroundColor="#ffafa5";
+}
+function img7(){
+  document.getElementById('changing').src="/public/images/Accordion/pro_plank_02_ui@2x.png";
+  document.getElementById('sideimg').style.backgroundColor="#fad24b";
+}
+function img8(){
+  document.getElementById('changing').src="/public/images/Accordion/homepage-static-fallback-alt.jpg";
+  document.getElementById('sideimg').style.backgroundColor="#ff8c19";
+}
+
+
+
+
+
+
+
+
+
+
+
+// carousal number
